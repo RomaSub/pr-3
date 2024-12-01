727c679bb4c67951d929174927721d64c23cb7b2
Roma <118161260+RomaSub@users.noreply.github.com>
2024-12-01 17:55:11 +0300

    .

--- app/(tabs)/about.tsx
+++ app/(tabs)/about.tsx
@@ -3,7 +3,7 @@ import { View, Text, StyleSheet } from "react-native";
 export default function AboutScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Мое Имя</Text>
+      <Text style={styles.text}>Информация обо мне</Text>
     </View>
   );
 }
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,30 +1,79 @@
-import { Text, View, StyleSheet } from "react-native";
-import { Link } from "expo-router";
+import React from "react";
+import { FlatList, StyleSheet, Text, View } from "react-native";
+
+const langs = [
+  {
+    id: "1",
+    experience: "2 года",
+    lang: "JavaScript",
+  },
+  {
+    id: "2",
+    experience: "1 год",
+    lang: "TypeScript",
+  },
+  {
+    id: "3",
+    experience: "5 месяцев",
+    lang: "Go",
+  },
+  {
+    id: "4",
+    experience: "2 месяца",
+    lang: "Zig",
+  },
+];
 
 export default function Index() {
-  return (
+  const renderItem = ({ item }: { item: (typeof langs)[0] }) => (
     <View style={styles.container}>
-      <Text style={styles.text}>Home screen</Text>
-      <Link href="/about" style={styles.button}>
-        Go to About screen
-      </Link>
+      <View style={styles.textContainer}>
+        <Text style={styles.langName}>{item.lang}</Text>
+        <Text style={styles.experience}>Опыт: {item.experience}</Text>
+      </View>
     </View>
+  );
+
+  return (
+    <FlatList
+      data={langs}
+      renderItem={renderItem}
+      keyExtractor={(item) => item.id}
+      contentContainerStyle={styles.list}
+    />
   );
 }
 
 const styles = StyleSheet.create({
+  list: {
+    padding: 16,
+  },
   container: {
-    flex: 1,
-    backgroundColor: "#25292e",
+    flexDirection: "row",
     alignItems: "center",
-    justifyContent: "center",
+    backgroundColor: "white",
+    borderRadius: 10,
+    padding: 15,
+    marginVertical: 8,
+    marginHorizontal: 16,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.23,
+    shadowRadius: 2.62,
+    elevation: 4,
   },
-  text: {
-    color: "#fff",
+  textContainer: {
+    flex: 1,
   },
-  button: {
-    fontSize: 20,
-    textDecorationLine: "underline",
-    color: "#fff",
+  langName: {
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+  experience: {
+    fontSize: 14,
+    color: "gray",
   },
 });
